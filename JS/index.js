969fba53fe1ad0a3eafc30a741082cd184f8497c
carlosvldz <me@carlosvldz.com>
2020-08-08 13:54:27 -0500

    add uberPool in  index.js and the script for uberPool in the HTML file

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,11 @@
-var car = new Car("AW456", new Account("Andes Herrera", "WER123"))
+var car = new Car("AW456", new Account("Andres Herrera", "QWE234"))
 car.passenger = 4
 car.printDataCar()
 
-var uberX = new uberX("AW456", new Account("Andes Herrera", "WER123"), "FIAT", "PALIO")
+var uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark")
 uberX.passenger = 4
 uberX.printDataCar()
+
+var uberPool = UberPool("AW123", new Account("Carlos Sanchez", "TYU234"), "Volkswagen", "Jetta")
+uberPool.passenger = 2
+uberPool.printDataCar()
